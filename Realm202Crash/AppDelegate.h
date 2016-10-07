faeb96ce1e7bf97624498c455fbe740d626b0ded
Cassiano Monteiro <cassianomonteiro@gmail.com>
2016-10-07 15:04:46 -0300

    Added comments to reproduce crash.

--- Realm202Crash/AppDelegate.h
+++ Realm202Crash/AppDelegate.h
@@ -12,8 +12,10 @@
 // @link https://github.com/realm/realm-cocoa/issues/4190
 
 // To recriate the crash:
-// 1. Run unit tests
-// 2. Run the app on Simulator (this issue was run on iPhone 4S with iOS 9.3 Simulator)
+// 1. Run pod install (used cocoapods 1.0.1)
+// 2. Open this project's workspace
+// 3. Run unit tests
+// 4. Run the app on Simulator (this issue was run on iPhone 4S with iOS 9.3 Simulator)
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 @property (strong, nonatomic) UIWindow *window;
